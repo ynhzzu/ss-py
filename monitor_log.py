8cedab43eb4a5eb9a4d856e3701d898b8acd5402
ynhzz <ynhzzu@gmail.com>
2016-07-07 11:06:45 +0800

    update log format

--- monitor_log.py
+++ monitor_log.py
@@ -7,7 +7,7 @@ import redis
 
 #logs = tailer.tail(open('/var/log/shadowsocks.log'), 10)
 logs = tailer.follow(open('/var/log/shadowsocks.log'))
-pattern = re.compile(r'([0-9]{4}-[0-9]{2}-[0-9]{2}) ([0-9]{2}:[0-9]{2}:[0-9]{2}) INFO     host:([^ ]+) port:([0-9]+) connecting ([^ ]+) from ([^ ]+) ip:::ffff:([^ ]+)')
+pattern = re.compile(r'([0-9]{4}-[0-9]{2}-[0-9]{2}) ([0-9]{2}:[0-9]{2}:[0-9]{2}) INFO     host:([^ ]+) port:([0-9]+) connecting ([^ ]+) from ([^ ]+) ip:([^ ]+)')
 for line in logs:
     match = pattern.match(line)
     r = redis.StrictRedis(host = '127.0.0.1', port = 6379, db = 0, password = 'root123')
